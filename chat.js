214f9d423a08a33e2ef9efd179a5b1840858be3a
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2025-12-18 13:47:01 +0200

    old messages load

--- chat.js
+++ chat.js
@@ -1,20 +1,40 @@
-const now = new Date()
-const customTime = now.toLocaleTimeString('en-US', { 
-    hour: '2-digit', 
-    minute: '2-digit', 
-    hour12: false 
-}); 
 const token = localStorage.getItem("access_token")
+var chatMessages = document.getElementById("messages");
+window.onload = function getMessages() {
+	fetch("http://127.0.0.1:8000/getmessages", {
+		method:'GET',
+		headers: {
+			"Authorization": `Bearer ${token}`,
+			"Accept": 'application/json, text/plain, */*',
+                        "Content-type": 'application/json'
+
+		}
+	})
+  	.then(res => {
+        	return res.json().then(data => ({ status: res.status, data: data }));
+        })
+	.then(({ status, data }) => {
+		if (status === 200) {
+			data.forEach(function(message) {
+				chatMessages.insertAdjacentHTML('beforeend', `
+					<div id="message">
+						<li class="text">${message.message_content}</li>
+					</div>
+				`)
+			}) 
+		}	
+	})
+}
 const displayName = localStorage.getItem('display_name');
 document.querySelector("#ws-id").textContent = displayName
 var ws = new WebSocket(`ws://localhost:8000/ws?token=${token}`);
 
 ws.onmessage = function(event) {
-	var messages = document.getElementById('messages');
-	var message = document.createElement('li');
-	var content = document.createTextNode(event.data);
-	message.appendChild(content);
-	messages.appendChild(message);
+	chatMessages.insertAdjacentHTML('beforeend', `
+		<div id="message">
+			<li class="text">${event.data}</li>
+		</div>
+	`)
 };
 
 function sendMessage(event) {
