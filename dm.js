034f5faec756982f2e1dd0c6bada80c1d575d222
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2025-12-23 06:01:15 +0200

    dms working (except for loading messages)

--- dm.js
+++ dm.js
@@ -1,8 +1,22 @@
 const token = sessionStorage.getItem("access_token");
 var chatMessages = document.getElementById("messages");
+const messageForm = document.getElementById("message-form");
+
+const displayName = sessionStorage.getItem('display_name');
+document.querySelector("#ws-id").textContent = displayName
+
 
 const target = sessionStorage.getItem("target");
 var dm = new WebSocket(`ws://localhost:8000/wsdm?token=${token}&target=${target}`);
+
+
+messageForm.addEventListener("submit", function sendDm(event) {
+	event.preventDefault();
+	var input = document.getElementById('messageText');
+	dm.send(input.value);
+	input.value = '';
+});
+
 
 dm.onmessage = function(event) {
 	chatMessages.insertAdjacentHTML('beforeend', `
@@ -14,10 +28,4 @@ dm.onmessage = function(event) {
 };
 
 
-function sendDm(event) {
-	event.preventDefault();
-	var input = document.getElementById('messageText');
-	dm.send(input.value);
-	input.value = '';
-}
 
